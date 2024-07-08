94751fb5505a445b770aedaaa4019746cbda7e90
1manasseh23 <cletavivan@gmail.com>
2024-07-09 00:56:29 +0300

    Measure the runtime

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -19,6 +19,6 @@ def measure_time(n: int, max_delay: int) -> float:
     start_time = time.time()
     wait_n(n, max_delay)
     end_time = time.time()
-    
+
     total_time = end_time - start_time
     return total_time / n
